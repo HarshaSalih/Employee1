df70dd8ea386f13f7c4d7fb6c0dd04ee64efdc78
HarshaSalih <harshasalih2000@gmail.com>
2022-11-22 06:06:58 +0530

    commits

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+class Employee {
+    int code;
+    String name;
+    String designation;
+    double salary;
+    String companyName;
+
+    long mobileNo;
+    String email;
+
+    public Employee(int code, String name, String designation, double salary, String companyName, long mobileNo, String email) {
+        this.code = code;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.companyName = companyName;
+        this.mobileNo = mobileNo;
+        this.email = email;
+    }
+
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("choose an option");
+        ArrayList<Employee> arrEmp = new ArrayList<Employee>();
+        while (true) {
+            System.out.println("Select an option \n 1 - Add Employee \n 2 - View Employee \n 3 - Search an Employee \n" +
+                    "4 - Delete an Employee \n 5 - Exit");
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            if (option == 5) {
+                System.out.println("You are exiting from the program");
+                System.exit(0);
+            } else {
+
+
+            }
+        }
     }
 }
